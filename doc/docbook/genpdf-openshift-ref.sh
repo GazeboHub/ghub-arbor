3c983b98b97edeb1b66d9df9a30ed45451bb1602
Sean Champ <spchamp+git-muletop@me.com>
2013-10-09 02:24:21 -0700

    do not ulink.show (PDF); move @xlink:href to link elements (inline)

--- doc/docbook/genpdf-openshift-ref.sh
+++ doc/docbook/genpdf-openshift-ref.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SSBASE=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns/
+SBASE=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns/
 
-exec saxon-xslt openshift-ref.m.xml $SSBASE/fo/docbook.xsl |
+exec saxon-xslt openshift-ref.m.xml $SBASE/fo/docbook.xsl ulink.show=0 |
 	fop - openshift-ref.m.pdf
