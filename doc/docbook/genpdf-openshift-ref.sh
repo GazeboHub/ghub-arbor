7901cdf5cf75b126ffff3029d6178b1ad90d85e0
Sean Champ <spchamp+git-muletop@me.com>
2013-10-09 11:29:40 -0700

    add overview table; small mods on PDF formatting flow

--- doc/docbook/genpdf-openshift-ref.sh
+++ doc/docbook/genpdf-openshift-ref.sh
@@ -2,5 +2,8 @@
 
 SBASE=/usr/share/xml/docbook/stylesheet/docbook-xsl-ns/
 
-exec saxon-xslt openshift-ref.m.xml $SBASE/fo/docbook.xsl ulink.show=0 |
+## cf http://docbook.sourceforge.net/release/xsl/1.78.0/doc/fo/
+
+exec saxon-xslt openshift-ref.m.xml $SBASE/fo/docbook.xsl \
+     ulink.show=0 segmentedlist.as.table=1 |
 	fop - openshift-ref.m.pdf
